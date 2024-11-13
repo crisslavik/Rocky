2c0bf51964860bfbeb5b20c994d6d9198944c8ff
crisslavik <98008457+crisslavik@users.noreply.github.com>
2024-11-13 15:12:44 -0800

    Update 4.install-KDE.sh
    personalized

--- 4.install-KDE.sh
+++ 4.install-KDE.sh
@@ -3,11 +3,12 @@
 APP_NAME="KDE"
 
 echo -e "\033[0;32m====================================="
-echo -e "\033[1;32mThe Linux IT Guy - Rocky Linux 9.4 Install Script"
+echo -e "\033[1;32mNOX VFX - Rocky Linux 9.4 Install Script"
 echo -e "\033[1;32mInstalling $APP_NAME"
 echo -e "\033[0;32m=====================================\033[0m"
 
 # https://forums.rockylinux.org/t/nvidia-drivers-on-rocky-linux/12366
 # Install KDE
-sudo dnf groupinstall "KDE Plasma Workspaces" -y
-sudo dnf install sddm krdc kde-gtk-config konsole firefox sddm-breeze   -y
+sudo dnf install plasma-desktop -y
+sudo dnf install epel-release -y
+sudo dnf install sddm vscode dolphin slack plasma-systemmonitor gwenview kcalc krdc kde-gtk-config konsole firefox ark krfb sddm-breeze google-chrome -y
