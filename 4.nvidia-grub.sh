10594c8b70a9a6d243c8dfed6817e2d8f34f7ad8
crisslavik <98008457+crisslavik@users.noreply.github.com>
2025-01-10 22:20:57 -0800

    Update 4.nvidia-grub.sh
    updating the setup to enrol a Key

--- 4.nvidia-grub.sh
+++ 4.nvidia-grub.sh
@@ -10,5 +10,14 @@ echo -e "\033[0;32m=====================================\033[0m"
 
 # https://forums.rockylinux.org/t/nvidia-drivers-on-rocky-linux/12366
 #modify grub so nouveau doesn't conflict
-sudo grubby --update-kernel=ALL --args="rd.driver.blacklist=nouveau modprobe.blacklist=nouveau"
-sudo sed -i -e 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="rd.driver.blacklist=nouveau modprobe.blacklist=nouveau /g' /etc/default/grub
+#sudo grubby --update-kernel=ALL --args="rd.driver.blacklist=nouveau modprobe.blacklist=nouveau"
+#sudo sed -i -e 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="rd.driver.blacklist=nouveau modprobe.blacklist=nouveau /g' /etc/default/grub
+
+sudo mokutil --import /var/lib/dkms/mok.pub
+sudo reboot
+
+#After the reboot, your system should ask you if you want to enroll a key or something like that, say yes and it asks for the password you gave in the mokutil command.
+#Later on, check that the nvidia drivers are installed:
+#nvidia-smi
+
+#If you dual boot windows, a windows-driven firmware update may reset the TPM chip (?). Anyway, any time this happens you’ll need to re-run ‘mokutil --import /var/lib/dkms/mok.pub’
